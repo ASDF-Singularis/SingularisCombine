702af1199a58ab4866d6d764d542dd6484fc97da
TrifingZW <TrifingZW@gmail.com>
2026-08-01 13:57:56 +0800

    feat: 为 SingularisCombine 添加原生 GameplayTag 声明
    定义标签层级 Singularis.Combine、Singularis.Combine.Reaction 及 Singularis.Combine.Reaction.Default，供插件内游戏玩法逻辑引用。

---
+++ Source/SingularisCombine/Private/Types/SingularisCombineGameplayTags.cpp
@@ -0,0 +1,19 @@
+#include "Types/SingularisCombineGameplayTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+	SingularisCombine,
+	"Singularis.Combine",
+	"Singularis Combine"
+);
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+	SingularisCombine_Reaction,
+	"Singularis.Combine.Reaction",
+	"Singularis Combine Reaction"
+);
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+	SingularisCombine_Reaction_Default,
+	"Singularis.Combine.Reaction.Default",
+	"Singularis Combine Reaction Default"
+);
---
+++ Source/SingularisCombine/Public/Types/SingularisCombineGameplayTags.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <NativeGameplayTags.h>
+
+/**
+ * Singularis Combine 插件使用的原生 GameplayTag 声明。
+ *
+ * 标签层级：
+ *   Singularis.Combine
+ *   Singularis.Combine.Reaction
+ *   Singularis.Combine.Reaction.Default
+ */
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(SingularisCombine);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(SingularisCombine_Reaction);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(SingularisCombine_Reaction_Default);
